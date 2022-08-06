22d5f7fb7fbc682e3d0cdf839752eaf04b17812e
Matt <matthewbuckley051@gmail.com>
2022-08-06 17:04:18 +0100

    Added CLI
    Added search command which expects a folder to search and has
    parameters for turning off recursive search and defining the output file.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,58 @@
-use std::{env, path::PathBuf};
+use std::path::PathBuf;
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
+use clap::{Parser, Subcommand};
 
 use grm;
 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Find all repositories in a given folder and save their status to a file.
+    Search {
+        /// Folder to search for repositories in
+        folder: PathBuf,
+        /// Turn off recursively searching sub-folders
+        #[clap(short, long)]
+        non_recursive: bool,
+        /// CSV file to save found repositories and status to,
+        /// defaults to 'repo_status.csv' in given folder.
+        #[clap(short, long)]
+        output: Option<PathBuf>,
+    },
+}
+
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        return Err(anyhow!("expected argument for folder to check"));
-    }
+    let cli = Cli::parse();
 
-    let folder = PathBuf::from(&args[1]);
+    match &cli.command {
+        Commands::Search {
+            folder,
+            non_recursive,
+            output,
+        } => {
+            println!("Searching {}", folder.to_string_lossy());
 
-    println!("Searching {}", folder.to_string_lossy());
+            let mut count = 0;
+            let repos = grm::find_repositories(&folder, !non_recursive, &mut count)?;
+            println!("\nFound {} repositories", repos.len());
 
-    let mut count = 0;
-    let repos = grm::find_repositories(&folder, true, &mut count)?;
-    println!("\nFound {} repositories\n{}", repos.len(), "-".repeat(100));
+            let output = match output {
+                Some(o) => o.to_owned(),
+                None => folder.join("repo_status.csv"),
+            };
 
-    let path = PathBuf::from("repo_status.csv");
-
-    grm::write_status_csv(repos, &path)?;
-
-    println!("Written: {}", path.to_string_lossy());
+            grm::write_status_csv(repos, &output)?;
+            println!("Written: {}", output.to_string_lossy());
+        }
+    };
 
     Ok(())
 }
